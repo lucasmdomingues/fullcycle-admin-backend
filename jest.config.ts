b15dfeed72b77bd7a1a6891f20262c59130aaf75
Lucas Martins Domingues <lucasmdomingues.dev@gmail.com>
2025-12-08 13:52:59 -0300

    added jest and swc

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type { Config } from "jest";
+import type {Config} from 'jest';
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -92,7 +92,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -125,7 +127,7 @@ const config: Config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: "./src",
+  // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
@@ -178,7 +180,7 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.(t|j)sx?$": "@swc/jest",
+   '^.+\\.(t|j)sx?$': '@swc/jest'
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
--- src/domain/__tests__/category.entity.spec.ts
+++ src/domain/__tests__/category.entity.spec.ts
@@ -1,13 +1,91 @@
 import { Category } from "../category.entity.js";
 
 describe("Category Unit Tests", () => {
-  test("should create a category", () => {
-    const category = new Category({ name: "Movie" });
-    expect(category).toStrictEqual({
-      name: "Movie",
-      description: null,
-      isActive: true,
-      createdAt: expect.any(Date),
+  describe("should create a category using constructor", () => {
+    test("should create a category with only name", () => {
+      const category = new Category({
+        name: "Movie",
+      });
+
+      expect(category.category_id).toBeDefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    test("should create a category with name and description", () => {
+      const category = new Category({
+        name: "Movie",
+        description: "some description",
+      });
+
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBe("some description");
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    test("should create a category with name and is_active false", () => {
+      const category = new Category({
+        name: "Movie",
+        is_active: false,
+      });
+
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBeFalsy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    test("should create a category with all properties", () => {
+      const created_at = new Date();
+      const category = new Category({
+        name: "Movie",
+        description: "some description",
+        is_active: false,
+        created_at,
+      });
+
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBe("some description");
+      expect(category.is_active).toBeFalsy();
+      expect(category.created_at).toBe(created_at);
+    });
+  });
+
+  describe("should create a category using factory method", () => {
+    test("should create a category with only name", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+
+      expect(category.category_id).toBeDefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+  });
+
+  describe("should change category properties", () => {
+    test("should change name", () => {
+      const category = new Category({
+        name: "Movie",
+      });
+
+      category.changeName("Documentary")
+      expect(category.name).toBe("Documentary");
+    });
+
+    test("should change description", () => {
+      const category = new Category({
+        name: "Movie",
+        description: "some description",
+      });
+
+      category.changeDescription("new description");
+      expect(category.description).toBe("new description");
     });
   });
 });
